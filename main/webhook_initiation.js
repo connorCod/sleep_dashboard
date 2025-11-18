472650757a6b51d263f012578e6e27e6ed80c098
Connor Hruby <connorhruby@Connors-MacBook-Pro-3.local>
2025-11-17 22:34:30 -0700

    fixing webhook initiation

--- main/webhook_initiation.js
+++ main/webhook_initiation.js
@@ -1,6 +1,11 @@
-import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config();
 import fetch from 'node-fetch'; // if using CommonJS, require('node-fetch')
-const CALLBACK_URL = process.env.CALLBACK_URL
+
+console.log("CLIENT_ID:", process.env.OURA_CLIENT_ID);
+console.log("CLIENT_SECRET length:", process.env.OURA_CLIENT_SECRET?.length);
+console.log("CALLBACK_URL:", process.env.CALLBACK_URL);
+console.log("WEBHOOK_TOKEN length:", process.env.OURA_WEBHOOK_TOKEN?.length);
 
 const url = "https://api.ouraring.com/v2/webhook/subscription";
 
@@ -11,7 +16,7 @@ const headers = {
 };
 
 const body = {
-  callback_url: CALLBACK_URL,
+  callback_url: process.env.CALLBACK_URL,
   verification_token: process.env.OURA_WEBHOOK_TOKEN,
   event_type: "update",
   data_type: "sleep",
